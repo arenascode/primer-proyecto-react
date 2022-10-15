51196f99a10c6d92dbf22af9305ba0cc314e02d8
Miguel Arenas <migue.arenas91@gmail.com>
2022-10-14 22:14:00 -0300

    proceso de item detail 1

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -4,21 +4,70 @@ import { products } from "./Products";
 
 const ProductDetail = ({ item }) => {
   return (
-    <div className="hero min-h-screen bg-base-200">
-      <div className="hero-content flex-col lg:flex-row-reverse">
-        <img
-          src={item.img}
-          className="max-w-sm rounded-lg shadow-2xl"
-        alt=""/>
-        <div>
-          <h1 className="text-5xl font-bold">{item.name}</h1>
-          <p className="py-6">
-            {item.Description}
-          </p>
-          <button className="btn">Comprar</button>
+    <div className="containerProductDetail flex-1">
+      <div class="mx-auto mt-6 max-w-2xl sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:gap-x-8 lg:px-8">
+        <div class="aspect-w-3 aspect-h-4 hidden overflow-hidden rounded-lg lg:block">
+          <img
+            src={item.img}
+            alt="Two each of gray, white, and black shirts laying flat."
+            class="h-full w-full object-cover object-center"
+          />
         </div>
       </div>
-    </div>
+      {/* Product Info  */}
+      <div className="productInfo m-10 flex">
+      <div class="lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8 content-center">
+        <h1 class=" font-bold tracking-normal text-gray-900 sm:text-3xl m-auto text-justify">
+          {item.name}
+          </h1>
+          <div class="space-y-6">
+          <p class="font-medium text-justify text-gray-900 w-80 mt-9 tracking-wide">
+            {item.Description}
+          </p>
+          </div>
+        </div>
+
+
+      <div class="properties">
+        <h1 class="font-semibold text-gray-900">Propiedades</h1>
+
+        <div class="mt-4">
+          <ul class="list-disc space-y-2 pl-4 text-sm">
+            <li class="text-gray-400">
+              <span class="text-gray-600">Hand cut and sewn locally</span>
+            </li>
+
+            <li class="text-gray-400">
+              <span class="text-gray-600">
+                Dyed with our proprietary colors
+              </span>
+            </li>
+
+            <li class="text-gray-400">
+              <span class="text-gray-600">Pre-washed &amp; pre-shrunk</span>
+            </li>
+
+            <li class="text-gray-400">
+              <span class="text-gray-600">Ultra-soft 100% cotton</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="mt-10 w-80 text-justify">
+        <h2 class="text-sm font-medium text-gray-900">Details</h2>
+
+        <div class="mt-4 space-y-6">
+          <p class="text-sm text-gray-600">
+            The 6-Pack includes two black, two white, and two heather gray Basic
+            Tees. Sign up for our subscription service and be the first to get
+            new, exciting colors, like our upcoming &quot;Charcoal Gray&quot;
+            limited release.
+          </p>
+        </div>
+      </div>
+      </div>
+      </div>
   );
 }
 
